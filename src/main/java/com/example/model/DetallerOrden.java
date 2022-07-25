8a8c4f6546c8de1acaa5d0182d9f780a0f4a5916
cricket361 <leonbrayan899@gmail.com>
2022-07-25 18:53:22 -0500

    creacion de la estructura del proyecto y las clases model - prueba 1

---
+++ src/main/java/com/example/model/DetallerOrden.java
@@ -0,0 +1,85 @@
+package com.example.model;
+
+
+
+public class DetallerOrden {
+	private Integer id;
+	private String nombre;
+	private double cantidad;
+	private double precio;
+	private double total;
+
+	
+	public DetallerOrden() {
+		
+	}
+
+
+	public DetallerOrden(Integer id, String nombre, double cantidad, double precio, double total) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.cantidad = cantidad;
+		this.precio = precio;
+		this.total = total;
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public double getCantidad() {
+		return cantidad;
+	}
+
+
+	public void setCantidad(double cantidad) {
+		this.cantidad = cantidad;
+	}
+
+
+	public double getPrecio() {
+		return precio;
+	}
+
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+
+	public double getTotal() {
+		return total;
+	}
+
+
+	public void setTotal(double total) {
+		this.total = total;
+	}
+
+
+	@Override
+	public String toString() {
+		return "DetallerOrden [id=" + id + ", nombre=" + nombre + ", cantidad=" + cantidad + ", precio=" + precio
+				+ ", total=" + total + "]";
+	}
+	
+	
+}
